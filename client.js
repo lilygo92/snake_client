4a01b45cfd53d838152ac55dad8c5b9e409271b8
Lily Go <lilygo7192@gmail.com>
2023-03-15 14:35:27 -0700

    Moved setupInput and handleUserInput to separate file and exported

--- client.js
+++ client.js
@@ -25,22 +25,4 @@ const connect = function () {
   return conn;
 };
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  process.stdout.write(':3');
-  if (key === '\u0003') {
-    process.exit();
-  };
-}
-
-setupInput();
-module.exports = connect;
+module.exports = {connect};
