aa74f62c7493624b2f8c79b8b21d45a53d9f467c
Lily Go <lilygo7192@gmail.com>
2023-03-15 13:46:10 -0700

    Added two functions setupInput and handleUserInput

--- client.js
+++ client.js
@@ -1,5 +1,4 @@
 const net = require("net");
-
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
@@ -13,10 +12,10 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: Hi!");
-    setInterval(() => {
-      conn.write("Move: up");
+    // setInterval(() => {
+    //   conn.write("Move: up");
     
-    }, 100);
+    // }, 100);
   })
   
   conn.on("data", (data) => {
@@ -26,4 +25,22 @@ const connect = function () {
   return conn;
 };
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  process.stdout.write(':3');
+  if (key === '\u0003') {
+    process.exit();
+  };
+}
+
+setupInput();
 module.exports = connect;
